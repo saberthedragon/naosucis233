eae21444350242a64474baaf8fdfd194245d2da5
Kicho Naosu <saberthedragon@yahoo.com>
2023-03-16 21:38:07 -0700

    Mostly done. Stuck on linking 'Paginate Drop-Down' with Component; Stuck on Pagination giving 'All()'; Project put on 'Hold' for the moment

--- crud-app/app/Http/Livewire/Interactive/Products.php
+++ crud-app/app/Http/Livewire/Interactive/Products.php
@@ -6,8 +6,38 @@ use Livewire\Component;
 
 class Products extends Component
 {
+    public $search = "";
+    public $sortBy = "name";
+    public $direction = "asc";
+    public $per_page = 10;
+    public bool $perPageAll = true;
+
+
+    protected $queryString = [
+        "search" => ["except" => ""],
+        "sortBy" => ["except" => ""],
+        "direction" => ["except" => ""],
+    ];
+
+    public function doSort($field, $direction)
+    {
+        $this->sortBy = $field;
+        $this->direction = $direction;
+    }
+
     public function render()
     {
-        return view('livewire.interactive.products');
+
+        $results =  \App\Models\Product::query()->paginate($this->per_page);
+
+
+        $products =  \App\Models\Product::with('reviews')
+            ->where('name', 'like', "%$this->search%")
+            ->orWhere('item_number', 'like', "%$this->search%")
+            ->orderBy($this->sortBy, $this->direction);
+
+
+
+        return view('livewire.interactive.products', ['products' => $products->paginate(10)]);
     }
 }
